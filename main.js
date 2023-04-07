c91418ae45403a7c9d82182d725e81abcb3be625
swastiraj984 <93324612+swastiraj984@users.noreply.github.com>
2023-04-07 15:29:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+noseX=0;
+noseY=0;
+difference=0;
+leftWristX=0;
+rightWristX=0;
+
+function setup(){
+    canvas=createCanvas(500,500);
+    canvas.position(560,150);
+    video=createCapture(VIDEO);
+    video.size(500,500);
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log("poseNet is initialized");
+}
+function draw(){
+    background("#080808");
+    document.getElementById("square_side").innerHTML="Width and the height of the square= "+difference+"px.";
+    fill("#ffffff");
+    stroke("#ffffff");
+    square(noseX,noseY,difference);
+}
+function gotPoses(results){
+if(results.length>0){
+    console.log(results);
+    console.log("nosex="+results[0].pose.nose.x);
+    console.log("nosey="+results[0].pose.nose.y);
+    console.log("leftWristx="+results[0].pose.leftWrist.x);
+    console.log("rightWristx="+results[0].pose.rightWrist.x);
+    noseX=results[0].pose.nose.x;
+    noseY=results[0].pose.nose.y;
+    leftWristX=results[0].pose.leftWrist.x;
+    rightWristX=results[0].pose.rightWrist.x;
+    difference=floor(leftWristX-rightWristX);
+    console.log("difference= "+difference);
+}
+}
